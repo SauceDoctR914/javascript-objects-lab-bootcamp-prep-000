fb6409d37eba338d601052b3190cf2cc80028fe9
Gavin Soko <sokolofg@gmail.com>
2018-05-10 20:37:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function updateObjectWithKeyAndValue(object, key, value){
  var newRecipes = Object.assign({}, object, {[key] : value});
  return newRecipes;
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return objec;
+}
