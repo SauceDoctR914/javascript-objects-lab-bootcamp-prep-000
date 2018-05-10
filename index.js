0fed63145f2ed86fd9e5c307f9be6e9fd0421187
Gavin Soko <sokolofg@gmail.com>
2018-05-10 20:30:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {}; 
 function updateObjectWithKeyAndValue(object, key, value){
- var newRecipes = Object.assign({}, key: value);
+ var newRecipes = Object.assign({}, {key: value});
  return newRecipes;
 }
